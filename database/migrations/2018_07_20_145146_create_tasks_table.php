014407596569b7b9aedec60d4dcd5cfa5a6776a3
Ismail Shuaau <ismailshuaau@gmail.com>
2018-07-20 20:32:42 +0500

    companies, projects, tasks

---
+++ database/migrations/2018_07_20_145146_create_tasks_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('days');
+            $table->unsignedInteger('hours');
+
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->unsignedInteger('project_id');
+            $table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade');
+            $table->unsignedInteger('company_id')->references('id')->on('companies')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+        $table->dropForeign('tasks_project_id_foreign');
+        $table->dropForeign('tasks_project_id_foreign');
+        $table->dropForeign('tasks_company_id_foreign');
+    }
+}
